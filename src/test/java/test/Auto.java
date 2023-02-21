f9573c528f0427bf529b44e05a9cf14137c029f8
agarcia1607 <124928465+agarcia1607@users.noreply.github.com>
2023-02-21 07:34:39 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,46 +1,46 @@
 package test;
 public class Auto {
-    String modelo;
-    int precio;
-    Asiento[] asientos;
-    String marca;
-    Motor motor;
-    int registro;
-    static int cantidadCreados;
-    
-    public Auto(String modelo, int precio, Asiento[] asientos, String marca, Motor motor, int registro) {
-        this.modelo = modelo;
-        this.precio = precio;
-        this.asientos = asientos;
-        this.marca = marca;
-        this.motor = motor;
-        this.registro = registro;
-    }
-    
-    public int cantidadAsientos() {
-        int contador = 0;
-        for (Asiento a : asientos) {
-            if (a != null) {
-                contador++;
-            }
-        }
-        return contador;
-    }
-    
-    public String verificarIntegridad() {
-        int x = this.registro;
-        if (x == this.motor.registro) {
-            for (Asiento a : asientos) {
-                if (a != null) {
-                    if (a.registro == x) {
-                        return "Auto original";
-                    }
-                    else {
-                        return "Las piezas no son originales";
-                    }
-                }
-            }
-        }
-        return "Auto original";
-    }
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() 
+	{
+		int numAsientos = 0;
+		for (int i = 0 ; i < asientos.length; i++)
+		{
+			if (asientos[i] != null)
+			{
+				numAsientos++;
+			}
+			
+		}
+		return numAsientos;
+	}
+	
+	String verificarIntegridad() 
+	{
+		if (registro == motor.registro)
+		{
+			for (int i = 0; i < asientos.length; i++)
+			{
+				if (asientos[i] != null)
+				{
+					if(asientos[i].registro != registro)
+					{
+						return "Las piezas no son originales";
+					}
+				}
+			}
+			return "Auto original";
+		}else {
+			return "Las piezas no son originales";
+		}
+		
+	}
+
 }
